16c393a11123ba2f7de335341eea8cafca79671a
Paulo <p.dantas@outlook.com>
2023-08-31 14:56:19 -0300

    first commit

---
+++ Math/Class1.cs
@@ -0,0 +1,9 @@
+﻿namespace Math;
+
+public class Class1
+{
+    public static int Add(int pOne, int pTwo) => (pOne+pTwo);
+    public static int Subtract(int pOne, int pTwo) => (pOne - pTwo);
+    public static int Multiply(int pOne, int pTwo) => (pOne * pTwo);
+    public static int Divide(int pOne, int pTwo) => (pOne / pTwo);
+}
