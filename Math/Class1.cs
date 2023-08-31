31d6fbce5dc2a1a46bb86a36aaccbdbcbcf7192f
Paulo <p.dantas@outlook.com>
2023-08-31 18:15:06 -0300

    Automated Tests

--- Math/Class1.cs
+++ Math/Class1.cs
@@ -6,4 +6,9 @@ public class Class1
     public static int Subtract(int pOne, int pTwo) => (pOne - pTwo);
     public static int Multiply(int pOne, int pTwo) => (pOne * pTwo);
     public static int Divide(int pOne, int pTwo) => (pOne / pTwo);
+
+    public bool IsOddNumber(int number)
+    {
+        return number % 2 !=0;
+    }
 }
--- MathTests/UnitTest1.cs
+++ MathTests/UnitTest1.cs
@@ -5,11 +5,62 @@ namespace MathTests;
 
 public class UnitTest1
 {
-    [Fact]
-    public void Test1()
+    [Fact(DisplayName = "Given integer numbers, sum should succeed")]
+    public void Test_Add()
     {
-        var actual = Class1.Add(1,3);
-        Assert.Equal(4, actual);
+        //Arrange
+        const int resultExpected = 4;
+        const int pOne = 1;
+        const int pTwo = 3;
 
+        //act
+        var resultActual = Class1.Add(pOne,pTwo);
+        
+        //Assert
+        Assert.Equal(resultExpected, resultActual);
+    }
+    
+    [Fact(DisplayName = "Given integer numbers, subtract should succeed")]
+    public void Test_Subtract()
+    {
+         //Arrange
+        const int resultExpected = 4;
+        const int pOne = 9;
+        const int pTwo = 5;
+
+        //act
+        var resultActual = Class1.Subtract(pOne,pTwo);
+        
+        //Assert
+        Assert.Equal(resultExpected, resultActual);
+    }
+    [Fact(DisplayName = "Given integer numbers, multiply should succeed")]
+    public void Test_Multiply()
+    {
+        //Arrange
+        const int resultExpected = 20;
+        const int pOne = 2;
+        const int pTwo = 10;
+
+        //act
+        var resultActual = Class1.Multiply(pOne,pTwo);
+        
+        //Assert
+        Assert.Equal(resultExpected, resultActual);
+    }
+    [Fact(DisplayName = "Given integer numbers, divide should succeed")]
+    public void Test_Divide()
+    {
+        //Arrange
+        const int resultExpected = 2;
+        const int pOne = 10;
+        const int pTwo = 5;
+
+        //act
+        var resultActual = Class1.Divide(pOne,pTwo);
+        
+        //Assert
+        Assert.Equal(resultExpected, resultActual);
+        
     }
 }
